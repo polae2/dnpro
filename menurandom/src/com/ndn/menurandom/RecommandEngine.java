838b9924119674901d1b50f1e868b3f6e24a768a
reaperes <reaperes105@gmail.com>
2012-06-26 14:27:19 +0900

    오류 1개 수정및 재 업

--- menurandom/src/com/ndn/menurandom/RecommandEngine.java
+++ menurandom/src/com/ndn/menurandom/RecommandEngine.java
@@ -13,7 +13,7 @@ public class RecommandEngine {
 		MenuData menuData = new MenuData();
 		menuData.name = "감자수프";
 		menuData.imgName = "a001";
-		menuData.searchName = "ㅁㅁ면4ㄷ챠5댜7ㅕㅇㅅ랴ㅏㅕ7ㄷ갸7ㄷ5갸76ㄷ갸7ㄷㄱ";
+		menuData.searchName = "수프";
 		menuData.explanation = "추운날 따뜻한 감자수프 어떠세요?";
 		
 		return menuData;
